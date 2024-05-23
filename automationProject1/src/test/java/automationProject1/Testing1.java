04712b124065b9b83b4581990c0ddf44336c6b17
Nahian Siddiqua <nahiansiddiqua301@gmail.com>
2024-05-23 11:42:39 +0600

    Second commit

--- automationProject1/src/test/java/automationProject1/Testing1.java
+++ automationProject1/src/test/java/automationProject1/Testing1.java
@@ -1,6 +1,8 @@
 package automationProject1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -35,10 +37,54 @@ public class Testing1 {
 	
 	@Test(priority=1)
 	
+	
 	public void TestURL(){
 		  String ExpectedUrl ="https://www.saucedemo.com/";
 	      String ActualUrl = driver.getCurrentUrl();
 	      System.out.println(ActualUrl);
 	      Assert.assertEquals(ActualUrl, ExpectedUrl, "URL Matched");
 			
-	}}
+	}
+
+	@Test(priority=3)
+	public void testUserNameAndPassword() {
+	    // Entering username and password
+	    driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	    driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	    driver.findElement(By.id("login-button")).click();
+	    
+	    String ExpectedUrl ="https://www.saucedemo.com/inventory.html";
+	    String ActualUrl = driver.getCurrentUrl();
+	    System.out.println(ActualUrl);
+	    Assert.assertEquals(ActualUrl, ExpectedUrl, "Login Successful");
+	}
+	
+	@Test(priority=4)
+	public void clickOnInventoryItem() {
+		
+	    driver.findElement(By.id("item_4_title_link")).click();
+	    
+	    String ExpectedTittle ="https://www.saucedemo.com/inventory-item.html?id=4";
+	    String ActualUrl = driver.getCurrentUrl();
+	    System.out.println(ActualUrl);
+	    Assert.assertEquals(ActualUrl, ExpectedTittle, "Valid");
+	}
+	
+	@Test(priority = 4)
+	public void addItemToCart() {
+		
+		driver.findElement(By.className("btn")).click();
+	    
+	    WebElement element = driver.findElement(By.className("btn"));
+	    String actualText = element.getText();
+	    
+	    String expectedText = "Remove";
+	    
+	    Assert.assertEquals(actualText, expectedText, "Button text match expected value.");
+	}
+
+
+}
+	
+	 
+
